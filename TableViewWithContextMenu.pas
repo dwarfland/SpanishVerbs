dc5aacbe66ac8deff8e9c525b203eec70eddfc5e
marc hoffman <mh@remobjects.com>
2022-12-28 14:04:57 -0400

    Regression with previous; clontext menu to set stem change & remove items; updated defaults;

---
+++ TableViewWithContextMenu.pas
@@ -0,0 +1,20 @@
+﻿namespace Verbs;
+
+type
+  [IBObject]
+  TableViewWithContextMenu = public class(NSTableView)
+  private
+
+    method menuForEvent(aEvent: not nullable NSEvent): NSMenu; public; override;
+    begin
+      inherited menuForEvent(aEvent);
+      var lRow := rowAtPoint(convertPoint(aEvent.locationInWindow) fromView(nil));
+      var lColumn := columnAtPoint(convertPoint(aEvent.locationInWindow) fromView(nil));
+      if (lColumn > -1) and (lRow > -1) then
+        if &delegate.respondsToSelector(selector(tableView:menuForTableColumn:row:)) then
+          exit id(&delegate).tableView(self) menuForTableColumn(tableColumns[lColumn]) row(lRow);
+    end;
+
+  end;
+
+end.
