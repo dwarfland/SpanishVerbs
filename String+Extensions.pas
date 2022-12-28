a3d6a79cf1c1f30953b6503aa98e7ee7835cad86
marc hoffman <mh@remobjects.com>
2022-12-28 11:47:59 -0400

    Implemented all tenses

--- String+Extensions.pas
+++ String+Extensions.pas
@@ -6,6 +6,30 @@ type
 
     property Last: Char read Chars[Length-1];
 
+    method AddAccentToLastChar: String;
+    begin
+      result := self;
+      if Length > 0 then
+        result := Substring(0, Length-1)+self[Length-1].AddAccent;
+    end;
+
+  end;
+
+  Char_Extensions = public extension class(Char)
+  public
+
+    method AddAccent: Char;
+    begin
+      result := case self of
+        'a': 'á';
+        'e': 'é';
+        'i': 'í';
+        'o': 'ó';
+        'u': 'ú';
+        else self;
+      end;
+    end;
+
   end;
 
 end.
--- Verb.pas
+++ Verb.pas
@@ -37,10 +37,23 @@ type
 
     property Stem: String read Infinitive.Substring(0, length(Infinitive)-2);
     property StemPlusVowel: String read Infinitive.Substring(0, length(Infinitive)-1);
-    property StemVowel: String read case Declination of
+
+    property AdjustedVowel_Present: String read case Declination of
         Declination.A: "a";
         Declination.E: "e";
         Declination.I: "e";
+      end;
+
+    property AdjustedVowel_Subjuntivo: String read case Declination of
+        Declination.A: "e";
+        Declination.E: "a";
+        Declination.I: "a";
+      end;
+
+    property PresentVosotros: String read case Declination of
+        Declination.A: "áis";
+        Declination.E: "éis";
+        Declination.I: "ís";
       end;
 
     property ChangedStem: String read begin
@@ -53,7 +66,18 @@ type
           end;
         end;
       end;
+    end;
 
+    property SubjuntivoStem: String read begin
+      result := conjugationsByName["Indicativo.Present.Singular.3"];
+      if length(result) > 0 then
+        result := result.Substring(0, length(result)-1); // subjunctive stem = yo form of present indicative minus o ending
+    end;
+
+    property SubjuntivoImperfectStem: String read begin
+      result := conjugationsByName["Indicativo.Preterite.Singular.3"];
+      if length(result) > 3 then // always will bem, but jic
+        result := result.Substring(0, length(result)-3); // Instead of using the infinitive for a stem, the imperfect subjunctive uses the third person plural of the preterite (minus the -ron)
     end;
 
     property conjugationsByName[aName: not nullable String]: nullable String
@@ -108,6 +132,7 @@ type
           "Participle": getParticiple(lSplit[1]);
           "Imperative": getImperative(lSplit[1]);
           "Indicativo": getIndicativo(lSplit.Skip(1).ToList);
+          "Subjuntivo": getSubjuntivo(lSplit.Skip(1).ToList);
           else nil;
         end;
       end;
@@ -139,7 +164,7 @@ type
               else if Declination in [Declination.E, Declination.I] then
                 result := lStem+"iendo"
               else
-                result := lStem+StemVowel+"ndo";
+                result := lStem+AdjustedVowel_Present+"ndo";
             end;
           "Past": begin
             end;
@@ -147,21 +172,29 @@ type
       end;
     end;
 
+    // https://www.spanishdict.com/guide/commands
 
     method getImperative(aCount: String): String;
     begin
-      if aCount in ["Singular", "Plural"] then begin
-        result := conjugationsByName["Indicativo.Present.Singular.3"];
-        if assigned(result) and (aCount = "Plural") then begin
-          var lLastChar := result.Substring(length(result)-1);
-          lLastChar := case lLastChar of
-            "a": "e";
-            "e": "a";
-          end;
-          result := result.Substring(0, length(result)-1)+lLastChar;
-        end;
+      case aCount of
+        "Tu", "Tú", "Singular": result := conjugationsByName["Indicativo.Present.Singular.3"];
+        "Usted", "Plural": result := conjugationsByName["Subjuntivo.Present.Singular.3"]; // To form both affirmative and negative usted commands, use the third-person singular form of the present subjunctive.
+            //{$HINT later: use the third-person singular form of the present subjunctive.}
+            //result := conjugationsByName["Indicativo.Present.Singular.3"];
+            //var lLastChar := result.Substring(length(result)-1);
+            //lLastChar := case lLastChar of
+              //"a": "e";
+              //"e": "a";
+            //end;
+            //result := result.Substring(0, length(result)-1)+lLastChar;
+          //end;
+        "Ustedes": result := conjugationsByName["Subjuntivo.Present.Plural.3"]; // To form both affirmative and negative ustedes commands, use the third-person plural form of the present subjunctive.
+        "Nosotros": result := conjugationsByName["Subjuntivo.Present.Plural.1"];// To form both affirmative and negative nosotros commands, use the nosotros form of the present subjunctive.
+        "AffirmativeVosotros": result := Infinitive.Substring(0, length(Infinitive)-1)+"d"; // To form affirmative vosotros commands, replace the ‐r at the end of the infinitive with a ‐d.
+        "NegativeVosotros": result := conjugationsByName["Subjuntivo.Present.Plural.2"]; // To form negative vosotros commands, use the vosotros form of the present subjunctive.
+        "AffirmativeVos": result := Infinitive.Substring(0, length(Infinitive)-1).AddAccentToLastChar; // To form affirmative vos commands, drop the -r from the end of the infinitive and add an accent on the last vowel.
+        "NegativeVos": result := conjugationsByName["Subjuntivo.Present.Singular.2"]; // To form negative vos commands, use the tú form of the present subjunctive.
       end;
-
     end;
 
     method getIndicativo(aSplit: List<String>): String;
@@ -169,7 +202,9 @@ type
       if aSplit.Count ≥ 0 then begin
         result := case aSplit[0] of
           "Present": getIndicativoPresent(aSplit.Skip(1).ToList);
-          "Past": getIndicativoPast(aSplit.Skip(1).ToList);
+          "Preterite": getIndicativoPreterite(aSplit.Skip(1).ToList);
+          "Imperfect": getIndicativoImperfect(aSplit.Skip(1).ToList);
+          "SimpleFuture": getIndicativoSimpleFuture(aSplit.Skip(1).ToList);
           else nil;
         end;
       end;
@@ -193,22 +228,199 @@ type
                 else
                   result := ChangedStem+"o";
               end;
-            "2": result := ChangedStem+StemVowel+"s";
-            "3": result := ChangedStem+StemVowel;
+            "2": result := ChangedStem+AdjustedVowel_Present+"s";
+            "3": result := ChangedStem+AdjustedVowel_Present;
           end;
           "Plural": case aSplit[1] of
             "1": result := StemPlusVowel+"mos"; // no stem change *and* no vowel change for 1st plural
-            //"2": result := StemPlusVowel+"s";
-            "3": result := ChangedStem+StemVowel+"n";
+            "2": result := Stem+PresentVosotros;
+            "3": result := ChangedStem+AdjustedVowel_Present+"n";
           end;
         end;
       end;
     end;
 
-    method getIndicativoPast(aSplit: List<String>): String;
+    // https://www.spanishdict.com/guide/spanish-preterite-tense-forms
+
+    method getIndicativoPreterite(aSplit: List<String>): String;
     begin
+      if (aSplit.Count = 2) then begin
+        case aSplit[0] of
+          "Singular": case aSplit[1] of
+            "1": result := ChangedStem+case Declination of
+                    Declination.A: "é";
+                    Declination.E: "í";
+                    Declination.I: "í";
+                  end;
+            "2": result := ChangedStem+case Declination of
+                    Declination.A: "aste";
+                    Declination.E: "iste";
+                    Declination.I: "iste";
+                  end;
+            "3": result := ChangedStem+case Declination of
+                    Declination.A: "ó";
+                    Declination.E: "ío";
+                    Declination.I: "í";
+                  end;
+          end;
+          "Plural": case aSplit[1] of
+            "1": result := StemPlusVowel+"mos"; // same as present!?
+            "2": result := ChangedStem+case Declination of
+                    Declination.A: "asteis";
+                    Declination.E: "isteis";
+                    Declination.I: "isteis";
+                  end;
+            "3": result := ChangedStem+case Declination of
+                    Declination.A: "aron";
+                    Declination.E: "ieron";
+                    Declination.I: "ieron";
+                  end;
+          end;
+        end;
+      end;
     end;
 
+    // https://www.spanishdict.com/guide/spanish-imperfect-tense-forms
+
+    method getIndicativoImperfect(aSplit: List<String>): String;
+    begin
+      if Declination in [Declination.A, Declination.E, Declination.I] then begin
+
+        if (aSplit.Count = 2) then begin
+          case aSplit[0] of
+            "Singular": case aSplit[1] of
+              "1": result := Stem+case Declination of
+                  Declination.A: "aba";
+                  Declination.E: "ía";
+                  Declination.I: "ía";
+                end;
+              "2": result := Stem+case Declination of
+                  Declination.A: "abas";
+                  Declination.E: "ías";
+                  Declination.I: "ías";
+                end;
+              "3": result := Stem+case Declination of
+                  Declination.A: "aba";
+                  Declination.E: "ía";
+                  Declination.I: "ía";
+                end;
+              end;
+            "Plural": case aSplit[1] of
+              "1": result := Stem+case Declination of
+                  Declination.A: "ábamos";
+                  Declination.E: "íamos";
+                  Declination.I: "íamos";
+                end;
+              "2": result := Stem+case Declination of
+                  Declination.A: "abais";
+                  Declination.E: "íais";
+                  Declination.I: "íais";
+                end;
+              "3": result := Stem+case Declination of
+                  Declination.A: "aban";
+                  Declination.E: "ían";
+                  Declination.I: "ían";
+                end;
+            end;
+          end;
+        end;
+      end;
+    end;
+
+    method getIndicativoSimpleFuture(aSplit: List<String>): String;
+    begin
+      if (aSplit.Count = 2) then begin
+        case aSplit[0] of
+          "Singular": case aSplit[1] of
+              "1": result := Infinitive+"é";
+              "2": result := Infinitive+"ás";
+              "3": result := Infinitive+"á";
+            end;
+          "Plural": case aSplit[1] of
+              "1": result := Infinitive+"emos";
+              "2": result := Infinitive+"éis";
+              "3": result := Infinitive+"án";
+            end;
+        end;
+      end;
+    end;
+    //
+    //
+    //
+
+    method getSubjuntivo(aSplit: List<String>): String;
+    begin
+      if aSplit.Count ≥ 0 then begin
+        result := case aSplit[0] of
+          "Present": getSubjuntivoPresent(aSplit.Skip(1).ToList);
+          "Imperfect": getSubjuntivo1Imperfect(aSplit.Skip(1).ToList); // we only use Subjuntivo 1, for now
+          else nil;
+        end;
+      end;
+    end;
+
+    // https://www.spanishdict.com/guide/spanish-present-subjunctive
+
+    method getSubjuntivoPresent(aSplit: List<String>): String;
+    begin
+      if (aSplit.Count = 2) then begin
+        case aSplit[0] of
+          "Singular": case aSplit[1] of
+            "1": result := SubjuntivoStem+AdjustedVowel_Subjuntivo;
+            "2": result := SubjuntivoStem+AdjustedVowel_Subjuntivo+"s";
+            "3": result := SubjuntivoStem+AdjustedVowel_Subjuntivo;
+          end;
+          "Plural": case aSplit[1] of
+            "1": result := SubjuntivoStem+AdjustedVowel_Subjuntivo+"mos";
+            "2": result := SubjuntivoStem+case Declination of
+                    Declination.A: "éis";
+                    Declination.E: "áis";
+                    Declination.I: "áis";
+                  end;
+            "3": result := SubjuntivoStem+AdjustedVowel_Subjuntivo+"n";
+          end;
+        end;
+      end;
+    end;
+
+    // https://www.spanishdict.com/guide/spanish-imperfect-subjunctive
+
+    method getSubjuntivo1Imperfect(aSplit: List<String>): String;
+    begin
+      if (aSplit.Count = 2) then begin
+        case aSplit[0] of
+          "Singular": case aSplit[1] of
+            "1": result := SubjuntivoImperfectStem+"ra";
+            "2": result := SubjuntivoImperfectStem+"ras";
+            "3": result := SubjuntivoImperfectStem+"ra";
+          end;
+          "Plural": case aSplit[1] of
+            "1": result := SubjuntivoImperfectStem.AddAccentToLastChar+"ramos";
+            "2": result := SubjuntivoImperfectStem+"rais";
+            "3": result := SubjuntivoImperfectStem+"ran";
+          end;
+        end;
+      end;
+    end;
+
+    method getSubjuntivo2Imperfect(aSplit: List<String>): String;
+    begin
+      if (aSplit.Count = 2) then begin
+        case aSplit[0] of
+          "Singular": case aSplit[1] of
+            "1": result := SubjuntivoImperfectStem+"se";
+            "2": result := SubjuntivoImperfectStem+"ses";
+            "3": result := SubjuntivoImperfectStem+"se";
+          end;
+          "Plural": case aSplit[1] of
+            "1": result := SubjuntivoImperfectStem.AddAccentToLastChar+"semos";
+            "2": result := SubjuntivoImperfectStem+"seis";
+            "3": result := SubjuntivoImperfectStem+"sen";
+          end;
+        end;
+      end;
+
+    end;
   end;
 
 method StemChangeToString(aStemChange: VerbStemChange): String;
